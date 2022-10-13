a557465a495844d99dab2a61c7892b54fda4bd1e
MacauleyStephenson <MacauleyStephenson@mail.com>
2022-10-13 11:07:39 +0100

    adding base JS for data and functionality, some scss added

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,28 +3,28 @@ const common = require('./webpack.common.js');
 const { merge } = require('webpack-merge');
 
 module.exports = merge(common, {
-	mode: 'development',
-	devtool: 'inline-source-map',
-	output: {
-		filename: 'main.js',
-		path: path.resolve(__dirname, 'dist'),
-	},
-	module: {
-		rules: [
-			{
-				test: /\.s[ac]ss$/i,
-				include: path.resolve(__dirname, 'src'),
-				use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
-			},
-		],
-	},
-	devServer: {
-		open: true,
-		hot: true,
-		compress: true,
-		historyApiFallback: true,
-		static: {
-			directory: path.join(__dirname, 'src'),
-		},
-	},
+  mode: 'development',
+  devtool: 'inline-source-map',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        include: path.resolve(__dirname, 'src'),
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+      },
+    ],
+  },
+  devServer: {
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+    static: {
+      directory: path.join(__dirname, 'src'),
+    },
+  },
 });
--- webpack.prod.js
+++ webpack.prod.js
@@ -5,28 +5,28 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = merge(common, {
-	mode: 'production',
-	output: {
-		filename: 'main.[contenthash].js',
-		path: path.resolve(__dirname, 'dist'),
-		assetModuleFilename: 'image/[name].[hash].[ext]',
-	},
-	plugins: [
-		new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' }),
-		new CleanWebpackPlugin(),
-	],
-	module: {
-		rules: [
-			{
-				test: /\.s[ac]ss$/i,
-				include: path.resolve(__dirname, 'src'),
-				use: [
-					MiniCssExtractPlugin.loader,
-					'css-loader',
-					'postcss-loader',
-					'sass-loader',
-				],
-			},
-		],
-	},
+  mode: 'production',
+  output: {
+    filename: 'main.[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'image/[name].[hash].[ext]',
+  },
+  plugins: [
+    new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        include: path.resolve(__dirname, 'src'),
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
 });
