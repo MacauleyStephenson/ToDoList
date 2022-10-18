e762030d58333e1b92371669cddb7c7133420654
MacauleyStephenson <MacauleyStephenson@mail.com>
2022-10-18 13:13:29 +0100

    getting imports/main inded js file adjusted

---
+++ src/model.js
@@ -0,0 +1,9 @@
+'use: strict';
+
+export default class Model {
+	constructor() {
+		this.tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+		this.projects = JSON.parse(localStorage.getItem('projects')) || [
+			{ id: 1, name: 'uncategorised' },
+		];
+	}
