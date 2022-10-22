ae26d7c51cc31759285f6453d20dbb95c6c37563
MacauleyStephenson <MacauleyStephenson@mail.com>
2022-10-22 12:09:36 +0100

    readded eslint

---
+++ src/view.js
@@ -0,0 +1,24 @@
+'use: strict';
+
+import sprite from './assets/sprite.svg';
+
+
+export default class View {
+	static createEle(...args) {
+		const [ele, ...styles] = args;
+		const element = document.createElement(ele);
+		styles.forEach((style) => element.classList.add(style));
+		return element;
+	}
+
+	static createSVG(...args) {
+		const [icon, ...styles] = args;
+		const w3ns = 'http://www.w3.org/2000/svg';
+		const svg = document.createElementNS(w3ns, 'svg');
+		const use = document.createElementNS(w3ns, 'use');
+		use.setAttribute('href', `${sprite}#icon-${icon}`);
+		styles.forEach((style) => svg.classList.add(style));
+		svg.append(use);
+		return svg;
+	}
+}
